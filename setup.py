45e8c9b0a43a22d1db302a5bc273e802d5b7dd7d
Antoine Pietri <antoine.pietri1@gmail.com>
2018-10-05 12:54:51 +0200

    Add requirements-test.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
+import os
 from setuptools import setup, find_packages
 
 
-def parse_requirements():
+def parse_requirements(name=None):
+    if name:
+        reqf = 'requirements-%s.txt' % name
+    else:
+        reqf = 'requirements.txt'
+
     requirements = []
-    for reqf in ('requirements.txt', 'requirements-swh.txt'):
-        with open(reqf) as f:
-            for line in f.readlines():
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                requirements.append(line)
+    if not os.path.exists(reqf):
+        return requirements
+
+    with open(reqf) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
     return requirements
 
 
-# Edit this part to match your module
-# full sample: https://forge.softwareheritage.org/diffusion/DCORE/browse/master/setup.py
+# Edit this part to match your module. full sample:
+# https://forge.softwareheritage.org/diffusion/DCORE/browse/master/setup.py
 setup(
     name='swh.objstorage',
     description='Software Heritage Object Storage',
@@ -26,8 +34,9 @@ setup(
         'bin/swh-objstorage-add-dir',
         'bin/swh-objstorage-fsck'
     ],   # scripts to package
-    install_requires=parse_requirements(),
+    install_requires=parse_requirements() + parse_requirements('swh'),
     setup_requires=['vcversioner'],
+    extras_require={'testing': parse_requirements('test')},
     vcversioner={},
     include_package_data=True,
 )
