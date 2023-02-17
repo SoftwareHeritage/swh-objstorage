79ab35f4ffafbb8edf4c2a182feda165ed462034
Antoine Lambert <anlambert@softwareheritage.org>
2023-02-17 16:38:14 +0100

    mypy: Bump to 1.0.1 and fix new typing errors
    Related to swh/meta#4960

--- swh/objstorage/objstorage.py
+++ swh/objstorage/objstorage.py
@@ -10,6 +10,8 @@ import lzma
 from typing import Callable, Dict, Iterable, Iterator, List, Optional, Tuple, Union
 import zlib
 
+from typing_extensions import Protocol
+
 from swh.model import hashutil
 from swh.model.model import Sha1
 
@@ -67,7 +69,7 @@ class NullDecompressor:
         return b""
 
 
-class _CompressorProtocol:
+class _CompressorProtocol(Protocol):
     def compress(self, data: bytes) -> bytes:
         ...
 
@@ -75,7 +77,7 @@ class _CompressorProtocol:
         ...
 
 
-class _DecompressorProtocol:
+class _DecompressorProtocol(Protocol):
     def decompress(self, data: bytes) -> bytes:
         ...
 
@@ -85,17 +87,17 @@ class _DecompressorProtocol:
 decompressors: Dict[str, Callable[[], _DecompressorProtocol]] = {
     "bz2": bz2.BZ2Decompressor,  # type: ignore
     "lzma": lzma.LZMADecompressor,  # type: ignore
-    "gzip": lambda: zlib.decompressobj(wbits=31),  # type: ignore
-    "zlib": zlib.decompressobj,  # type: ignore
+    "gzip": lambda: zlib.decompressobj(wbits=31),
+    "zlib": zlib.decompressobj,
     "none": NullDecompressor,  # type: ignore
 }
 
 compressors: Dict[str, Callable[[], _CompressorProtocol]] = {
-    "bz2": bz2.BZ2Compressor,  # type: ignore
-    "lzma": lzma.LZMACompressor,  # type: ignore
-    "gzip": lambda: zlib.compressobj(wbits=31),  # type: ignore
-    "zlib": zlib.compressobj,  # type: ignore
-    "none": NullCompressor,  # type: ignore
+    "bz2": bz2.BZ2Compressor,
+    "lzma": lzma.LZMACompressor,
+    "gzip": lambda: zlib.compressobj(wbits=31),
+    "zlib": zlib.compressobj,
+    "none": NullCompressor,
 }
 
 
