7252119d04a30a9a3bc6718da4698073e93724ec
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-05-05 12:44:09 +0200

    winery: ensure the worker exit functions are called in more circumstances
    Since recent (?) gunicorn versions, looks like worker_exit is called
    rather than worker_int on graceful shutdowns.

--- swh/objstorage/backends/winery/gunicorn.py
+++ swh/objstorage/backends/winery/gunicorn.py
@@ -3,16 +3,28 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import logging
 import sys
 
 from swh.core.api.gunicorn_config import *  # noqa
 
+logger = logging.getLogger(__name__)
+
+
+def worker_exit(arbiter, worker):
+    logger.info("Calling worker_exit")
+    shutdown_storage_backend()
+
 
 def worker_int(worker):
+    logger.warning("Calling worker_int")
+    shutdown_storage_backend()
+
+
+def shutdown_storage_backend():
     """Run on_shutdown callback for storage when a worker is terminating"""
     if "swh.objstorage.api.server" not in sys.modules:
         return
-
     objstorage = sys.modules["swh.objstorage.api.server"].objstorage
 
     if on_shutdown := getattr(objstorage, "on_shutdown", None):
