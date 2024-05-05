8a794c8d929b8708637b1bf63173bfe4e7ccaeb3
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-05-05 12:45:49 +0200

    winery: introduce a throttled access logging filter
    Gunicorn access logs on RPC backends are pretty noisy by default, but
    grouping successful requests into merged logs would make that more
    bearable.
    
    To use it, stick this in the gunicorn logging config:
    
    ```
    filters:
      throttle_accesslog:
        (): swh.objstorage.backends.winery.gunicorn.ThrottledAccessLog
        interval: 60
        status_codes: [200]
    handlers:
      gunicorn.access:
        level: INFO
        filters: [throttle_accesslog]
        handlers: [<your handler>]
    ```

--- swh/objstorage/backends/winery/gunicorn.py
+++ swh/objstorage/backends/winery/gunicorn.py
@@ -3,8 +3,11 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+from collections import Counter
 import logging
 import sys
+from time import monotonic
+from typing import Tuple
 
 from swh.core.api.gunicorn_config import *  # noqa
 
@@ -29,3 +32,57 @@ def shutdown_storage_backend():
 
     if on_shutdown := getattr(objstorage, "on_shutdown", None):
         on_shutdown()
+
+
+class ThrottledAccessLog(logging.Filter):
+    """Throttle gunicorn access log lines for `status_codes` to at most one
+    every `interval` seconds"""
+
+    def __init__(self, interval: int = 60, status_codes: Tuple[int, ...] = (200,)):
+        super().__init__(name="gunicorn.access")
+        self.status_codes = set(str(code) for code in status_codes)
+        self.endpoints: Counter[str] = Counter()
+        self.interval = interval
+
+        self.previous_flush = monotonic()
+        self.deadline = self.previous_flush + self.interval
+
+    def filter(self, record):
+        # gunicorn.access records are using `s` for status code and `U` for the
+        # requested path
+        if not (ret := super().filter(record)):
+            return ret
+
+        if record.args["s"] not in self.status_codes:
+            return True
+
+        # If we quiesce different status codes, stick them in different buckets
+        if len(self.status_codes) > 1:
+            bucket = f"{record.args['U']}({record.args['s']})"
+        else:
+            bucket = record.args["U"]
+
+        self.endpoints[bucket] += 1
+
+        now = monotonic()
+        if self.deadline > now:
+            # Quiesce record
+            return False
+
+        # logging.Filter must mutate the log record instead of creating a
+        # new one, up to and including Python 3.11
+        record.msg = (
+            "Served %(total)s requests in the last %(interval).1fs, "
+            "including %(most_common)s"
+        )
+        record.args = {
+            "total": self.endpoints.total(),
+            "most_common": ", ".join(
+                f"{v} {path}" for path, v in self.endpoints.most_common(3)
+            ),
+            "interval": now - self.previous_flush,
+        }
+        self.previous_flush = now
+        self.deadline = now + self.interval
+        self.endpoints = Counter()
+        return True
